ca213e430171ab170738970e0e552395412466c0
jomakun <jomamiwa@gmail.com>
2022-11-14 00:41:56 +0900

    221111

---
+++ 221111/typical90_bc.py
@@ -0,0 +1,20 @@
+#import itertools
+
+n, p, q = map(int, input().split())
+a = list(map(int, input().split()))
+ans = 0
+
+"""for i in itertools.combinations(a, 5):
+    temp = sum(i)
+    if temp % p == q:
+        ans += 1"""
+
+for i in range(n-4):
+    for j in range(i+1, n-3):
+        for k in range(j+1, n-2):
+            for l in range(k+1, n-1):
+                for m in range(l+1, n):
+                    if a[i]%p*a[j]%p*a[k]%p*a[l]%p*a[m]%p == q:
+                        ans += 1
+
+print(ans)
